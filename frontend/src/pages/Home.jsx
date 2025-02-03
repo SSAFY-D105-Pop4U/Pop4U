35ed82717fdff5b667e94c3157cb708e327fa008
leesanghyuk <leedltkdgur@naver.com>
2025-02-03 15:09:15 +0900

    feat : 메인 페이지 구현현

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="header">
+        <h2>
+          팝업 스토어
+          <br />
+          어디로 가볼까요?
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
