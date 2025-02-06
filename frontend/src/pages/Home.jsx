7eb7020d75a7a8a0fd882e31878f03e3fd15f3b6
이상혁 <leedltkdgur@naver.com>
2025-02-06 09:31:52 +0900

    Feat : 메인 가로 스크롤 컴포넌트 구현

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import HomeArea from "../components/HomeArea";
 import HomeHeader from "../components/HomeHeader";
+import HomeHorizScroll from "../components/HomeHorizScroll";
 import "../styles/Home.css";
 
 const Home = () => {
@@ -14,6 +15,16 @@ const Home = () => {
         </h2>
         <HomeArea />
       </div>
+
+      <div className="divider1"></div>
+      <h3>#새로 생긴 팝업스토어 ✨</h3>
+      <HomeHorizScroll />
+      <div className="divider2"></div>
+      <h3>#마감 임박 팝업스토어 ✨</h3>
+      <HomeHorizScroll />
+      <div className="divider2"></div>
+      <h3>#요즘 뜨는 팝업스토어 ✨</h3>
+      <HomeHorizScroll />
     </div>
   );
 };
