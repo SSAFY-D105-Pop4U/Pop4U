33919e8d9ea120f8cdee7b8a9b9e169e1c3e2eed
나용성 <ahn3931@naver.com>
2025-02-08 19:20:51 +0900

    Feat : S3 구현 (일단 review)

--- backend/pop4u/src/main/java/com/d105/pop4u/global/config/WebConfig.java
+++ backend/pop4u/src/main/java/com/d105/pop4u/global/config/WebConfig.java
@@ -1,11 +1,28 @@
 package com.d105.pop4u.global.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    @Autowired
+    public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
+        this.octetStreamReadMsgConverter = octetStreamReadMsgConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(octetStreamReadMsgConverter);
+    }
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
