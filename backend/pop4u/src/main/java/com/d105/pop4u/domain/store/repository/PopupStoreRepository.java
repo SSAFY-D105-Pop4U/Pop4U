f4fa82c740a16b4d15428895e8a7e56e91a54cf1
정민규 <ktfig7023@naver.com>
2025-02-11 14:16:33 +0900

    Feat : 팝업 전체 채팅방 생성

--- backend/pop4u/src/main/java/com/d105/pop4u/domain/store/repository/PopupStoreRepository.java
+++ backend/pop4u/src/main/java/com/d105/pop4u/domain/store/repository/PopupStoreRepository.java
@@ -3,6 +3,7 @@ package com.d105.pop4u.domain.store.repository;
 
 import com.d105.pop4u.domain.store.entity.PopupStore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,7 @@ public interface PopupStoreRepository extends JpaRepository<PopupStore, Long> {
     List<PopupStore> findTop10ByOrderByPopupStartDateDesc();
     List<PopupStore> findTop10ByOrderByPopupEndDateDesc();
     List<PopupStore> findTop10ByOrderByPopupViewCountDesc();
+    // ✅ 채팅방이 없는 팝업스토어 찾기
+    @Query("SELECT p FROM PopupStore p WHERE p.popupId NOT IN (SELECT c.popupStore.popupId FROM ChatRoom c)")
+    List<PopupStore> findPopupsWithoutChatRooms();
 }
