0d76bd2db8d712eb39d99426e830bc1eb0c0048b
김정모 <jjmm112366@gmail.com>
2025-02-13 16:24:25 +0900

    fix: /api 테스트

--- backend/pop4u/src/main/java/com/d105/pop4u/domain/store/controller/PopupStoreController.java
+++ backend/pop4u/src/main/java/com/d105/pop4u/domain/store/controller/PopupStoreController.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/popup")
+@RequestMapping("api/popup")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class PopupStoreController {
