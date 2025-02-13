59780cc8c35d3de909b66bb92a76e4de901c124b
이상혁 <leetkdgurdl@naver.com>
2025-02-14 00:27:02 +0900

    Feat : 인생네컷 화면 구현

--- frontend/src/pages/LifeShot.jsx
+++ frontend/src/pages/LifeShot.jsx
@@ -5,7 +5,8 @@ import LifeShotFrame from "../components/lifeShotPage/LifeShotFrame";
 import ShotToggleButton from "../components/lifeShotPage/ShotToggleButton";
 import ColorPicker from "../components/lifeShotPage/ColorPicker";
 import '../styles/pages/LifeShot.css';
-
+import DraggableGallery from "../components/lifeShotPage/DraggableGallery";
+import ShotIcon from "../components/lifeShotPage/ShotIcon"; 
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 
 const LifeShot = () => {
@@ -15,6 +16,7 @@ const LifeShot = () => {
     const [active, setActive] = useState("사진");
     const [selectedColor, setSelectedColor] = useState("#DDDDD");
     const [frameImages, setFrameImages] = useState(Array(4).fill(null));
+    const [selectedIcon, setSelectedIcon] = useState("");
 
     // 드래그가 끝났을 때 실행되는 핸들러
     const handleDragEnd = (result) => {
@@ -54,88 +56,98 @@ const LifeShot = () => {
         setImages((prev) => prev.filter((_, i) => i !== index));
       };
 
-  
-  
+    // 이모티콘 선택 핸들러 추가
+    const handleEmoticonSelect = (emoticon) => {
+        setSelectedIcon(emoticon);
+    };
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
-
-    
-    <div className="lifeshot-page">
-      <Header title={"인생네컷 제작"}/>
-      <LifeShotFrame 
-        selectedColor={selectedColor}
-        frameImages={frameImages}
-      />
-      
-      
-     
-      <ShotToggleButton active={active} setActive={setActive}/>
-
-      <Divider  height="2px" top="0px"/>
-    
-      {active === "사진" && (
-        <div className="lifeshot-image-section">
-            <Droppable droppableId="image-list" direction="horizontal">
-                {(provided) => (
-                    <div 
-                        className="lifeshot-image-scroll"
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                    >
-                        {images.map((image, index) => (
-                            <Draggable 
-                                key={index} 
-                                draggableId={`image-${index}`} 
-                                index={index}
-                            >
-                                {(provided, snapshot) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        className={`lifeshot-image-item ${
-                                            snapshot.isDragging ? 'dragging' : ''
-                                        }`}
-                                    >
-                                        <img 
-                                            src={image.preview} 
-                                            alt={`업로드 이미지 ${index + 1}`}
-                                        />
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </div>
-      )}
-      {active === "테마" && (
-        <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
-      )}
-     
-    
-          {/* ✅ 버튼 영역 */}
-      <div className="button-wrapper">
+      <div className="lifeshot-page">
+        <Header title={"인생네컷 제작"}/>
         
-                <label className="lifeshot-upload-btn">
-                    <input
-                    type="file"
-                    accept="image/*"
-                    multiple
-                    onChange={handleImageUpload}
-                    style={{ display: "none" }}
-                    />
-                    사진넣기
-                </label>
-                <button className="button">저장하기</button>
-            </div>
+        <div style={{ position: 'relative' }}>
+          <LifeShotFrame 
+            selectedColor={selectedColor}
+            frameImages={frameImages}
+          />
+          
+            <DraggableGallery selectedIcon={selectedIcon}/>
+          
         </div>
+        
+        <ShotToggleButton active={active} setActive={setActive}/>
+
+        <Divider  height="2px" top="0px"/>
+      
+        {active === "사진" && (
+          <div className="lifeshot-image-section">
+              <Droppable droppableId="image-list" direction="horizontal">
+                  {(provided) => (
+                      <div 
+                          className="lifeshot-image-scroll"
+                          ref={provided.innerRef}
+                          {...provided.droppableProps}
+                      >
+                          {images.map((image, index) => (
+                              <Draggable 
+                                  key={index} 
+                                  draggableId={`image-${index}`} 
+                                  index={index}
+                              >
+                                  {(provided, snapshot) => (
+                                      <div
+                                          ref={provided.innerRef}
+                                          {...provided.draggableProps}
+                                          {...provided.dragHandleProps}
+                                          className={`lifeshot-image-item ${
+                                              snapshot.isDragging ? 'dragging' : ''
+                                          }`}
+                                      >
+                                          <img 
+                                              src={image.preview} 
+                                              alt={`업로드 이미지 ${index + 1}`}
+                                          />
+                                      </div>
+                                  )}
+                              </Draggable>
+                          ))}
+                          {provided.placeholder}
+                      </div>
+                  )}
+              </Droppable>
+          </div>
+        )}
+        {active === "테마" && (
+          <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        )}{active === "캐릭터" && (
+          <ShotIcon 
+            selectedIcon={selectedIcon} 
+            setSelectedIcon={setSelectedIcon}
+            onSelectEmoticon={handleEmoticonSelect}
+          />
+        )}
+       
+      
+            {/* ✅ 버튼 영역 */}
+        <div className="button-wrapper">
+          
+                  <label className="lifeshot-upload-btn">
+                      <input
+                      type="file"
+                      accept="image/*"
+                      multiple
+                      onChange={handleImageUpload}
+                      style={{ display: "none" }}
+                      />
+                      사진넣기
+                  </label>
+                  <button className="button">저장하기</button>
+              </div>
+          </div>
         </DragDropContext>
       
-  
+    
   );
 };
 
