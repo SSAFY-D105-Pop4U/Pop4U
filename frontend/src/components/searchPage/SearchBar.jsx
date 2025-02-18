dc8c136ddfdda4c3f80e433ee87f17f9575c87de
김정모 <jjmm112366@gmail.com>
2025-02-18 17:52:34 +0900

    fix: 검색 바 수정

--- frontend/src/components/searchPage/SearchBar.jsx
+++ frontend/src/components/searchPage/SearchBar.jsx
@@ -1,38 +1,31 @@
 import "../../styles/components/SearchBar.css";
-import { useState,useEffect } from "react";
-import searchIcon from "../../assets/icons/search.svg";
+import { useState, useEffect } from "react";
+import { Search } from 'lucide-react'; // lucide-react 아이콘 사용
 import BackButton from "../../components/BackButton";
 
-const SearchBar = ({ onSearch ,searchQuery}) => {
+const SearchBar = ({ onSearch, searchQuery }) => {
   const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
-    onSearch(event.target.value); // 부모로 검색어 전달
+    onSearch(event.target.value);
   };
 
   useEffect(() => {
-    console.log("useEffect 실행됨 - 새로운 searchQuery 값:", searchQuery);
-    setInputValue(searchQuery || ""); // 값이 없으면 빈 문자열 설정
+    setInputValue(searchQuery || "");
   }, [searchQuery]);
-
-  const onClick = () => {
-    const newValue = "asdsa";
-    setInputValue(newValue);
-  };
 
   return (
     <div className="search-container">
       <BackButton />
-
       <div className="search-bar">
-        <img src={searchIcon} alt="search" className="search-icon" onClick={onClick} />
+        <Search className="search-icon" />
         <input
           type="text"
           placeholder="검색하기"
           className="search-input"
           value={inputValue}
-          onChange={handleInputChange} // 입력 값이 변경될 때 호출
+          onChange={handleInputChange}
         />
       </div>
     </div>
