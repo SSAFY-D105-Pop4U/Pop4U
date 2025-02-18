0142d96ee84e4b55753c54a105e78f15d8029ab2
이상혁 <leetkdgurdl@naver.com>
2025-02-18 17:13:08 +0900

    Feat : 상세화면 수정

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -6,23 +6,31 @@ const Info = ({ detail }) => {
   if (!detail) return <p>Loading...</p>; // ✅ detail이 null일 때 로딩 표시
 
   return (
-    <div>
+    <div className="detail-info-contain">
       <div className="info">
-        <div className="name">{detail.popupName}</div>
-        <div className="date">
+        
+        <div className="detial-title">
+          오픈 기간
+        </div>
+        <div className="detail-date">
           <div className="startdate">{detail.popupStartDate}~</div>
           <div className="enddate">{detail.popupEndDate}</div>
         </div>
-        <div className="address">
-          <img src={fullpin} alt="pin" className="pin" />
+
+        <div className="detial-title">
+          주소
+        </div>
+        <div className="detail-date">
+          {/* <img src={fullpin} alt="pin" className="pin" /> */}
           <div className="add">{detail.popupAddress}</div>
         </div>
+        <div className="detial-title">운영시간</div>
         <div className=" opentime">
-          <div className="time">운영시간</div>
-          <div className="popupOperationTime">{detail.popupOperationTime}</div>
+          
+          <div className="detail-date">{detail.popupOperationTime}</div>
         </div>
-        <div className="title">팝업스토어 소개</div>
-        <div className="des">{detail.popupDescription}</div>
+        <div className="detial-title" >팝업스토어 소개</div>
+        <div className="detail-date">{detail.popupDescription}</div>
       </div>
     </div>
   );
