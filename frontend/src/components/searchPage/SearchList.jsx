6232a4d70f998d3392705f5045eed5db6f153a19
김정모 <jjmm112366@gmail.com>
2025-02-19 14:57:26 +0900

    Fix: 검색 리스트 카드 디자인

--- frontend/src/components/searchPage/SearchList.jsx
+++ frontend/src/components/searchPage/SearchList.jsx
@@ -65,7 +65,7 @@ const SearchList = ({ searchQuery }) => {
             />
             <div className="result-info-overlay">
               <h3 className="result-title">{result.popupName}</h3>
-              <p className="result-time">{result.popupOperationTime}</p>
+              <p className="result-time">{result.popupStartDate}~{result.popupEndDate}</p>
             </div>
           </div>
         </div>
