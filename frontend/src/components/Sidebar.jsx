b5212df4fbfde592748b649a2b3e2a3a4e0fffe2
이상혁 <leetkdgurdl@naver.com>
2025-02-20 15:54:29 +0900

    Feat : 로그아웃 구현

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -28,7 +28,11 @@ const Sidebar = ({ isOpen, onClose }) => {
     setIsLoggedIn(isLogind); // ✅ 상태 변경은 useEffect에서 처리
   }, [isLogind]);;
   
-  
+  // 로그아웃
+  const handleLogOut = () => {
+    sessionStorage.clear();
+    setIsLoggedIn(false);
+  };
  
   if (!isOpen) return null;
 
@@ -165,7 +169,7 @@ const Sidebar = ({ isOpen, onClose }) => {
 
         {isLoggedIn && (
           <div className="logout-section">
-            <button className="logout-button">로그아웃</button>
+            <button className="logout-button" onClick={handleLogOut}>로그아웃</button>
           </div>
         )}
       </div>
