36cbd22294d1f3fb8d409521e5307169b7006c74
김동욱 <kkdw428@naver.com>
2025-02-21 01:12:42 +0900

    Fix : 모달창 수정

--- frontend/src/components/Game/GameResult.jsx
+++ frontend/src/components/Game/GameResult.jsx
@@ -70,13 +70,14 @@ const GameResult = ({ popupId, userId }) => {
         <div
           key={index}
           className="myranking animate__animated animate__fadeIn"
-          style={{ animationDelay: `${index * 0.2}s`,
-          color: player.rank === 1 ? "gold" : player.rank === 2 ? "silver" : player.rank === 3 ? "brown" : "black", // 🔥 등수별 색상 지정 }}
-        }}  >
+          style={{ animationDelay: `${index * 0.2}s` }}
+        >
           <strong>
-            {player.rank}등 {/* 🔥 1등이면 크라운 추가 */}
+            {player.rank}등
           </strong>
+          <strong>
           {player.nickname}
+          </strong>
         </div>
       ))}
     </div>
